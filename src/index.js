002b533a6c311248533ca5e55f094043014068db
Gaurav <gaurav.bhe.24@gmail.com>
2024-05-18 16:09:47 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./services/email_services');
+// const { sendBasicEmail } = require('./services/email_services');
+
+// const cron = require('node-cron');
+var cron = require('node-cron');
 
 const setupAndStartServer = () => {
     const app = express();
@@ -13,12 +16,16 @@ const setupAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`server Started at port ${PORT}`);
 
-        sendBasicEmail(
-            'support@admin.com',
-            'gauravbhendarkar123@gmail.com',
-            'This is a testing email',
-            'Hey, how are you, i hope you like the support'
-        );
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'gauravbhendarkar123@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you, i hope you like the support'
+        // );
+
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every two minutes');
+          });
     });
 }
 
