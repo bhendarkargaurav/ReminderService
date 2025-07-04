78721ebd9bb88d8d936e27297f858d4a790e8d67
Gaurav <gaurav.bhe.24@gmail.com>
2025-07-04 23:30:09 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -2,22 +2,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const { createChannel } = require('./utils/messageQueue');
+
 
 // const { sendBasicEmail } = require('./services/email_services');
 const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email_services');
 
 const jobs = require('./utils/job');
+const { subscribeMessage } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    // const channel = await createChannel();
+
     app.post('/api/v1/tickets', TicketController.create);
+
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
 
     app.listen(PORT, () => {
         console.log(`server Started at port ${PORT}`);
-        jobs();
+        // jobs();
+         
+
         // sendBasicEmail(
         //     'support@admin.com',
         //     'gauravbhendarkar123@gmail.com',
@@ -25,7 +37,6 @@ const setupAndStartServer = () => {
         //     'Hey, how are you, i hope you like the support'
         // );
 
-        
     });
 }
 
